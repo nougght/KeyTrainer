77b0ac9670ebd35dc72a9418c764d09fef162cb1
nougght <muhamedmakusev@gmail.com>
2025-02-20 21:10:20 +0300

    added json files for texts and words (english, russian)

--- control/data_control.py
+++ control/data_control.py
@@ -37,9 +37,9 @@ class KeyTrainerData:
             [" "]
         ]
         self.easy_text = ["Farmer Jack realized that big yellow quilts were expensive.",
-        "The quick brown fox jumps over the lazy dog"]
-        self.mid_text = ["Five jumping wizards hex bolty quick. Jacky can now give six big tips from the old quiz ",
-        "Is the milk cold, or still hot? Tom hasn't had anything to eat since breakfast."]
+        ]
+        self.mid_text = [,
+        ]
         self.hard_text = ["She's got eyes of the bluest skies As if they thought of rain I'd hate to look into those eyes and see an ounce of pain Her hair reminds me of a warm, safe place Where, as a child, I'd hide And pray for the thunder and the rain to quietly pass me by",
         "They decided to plant a tree in the backyard to celebrate the start of spring. Your name isn't you. That is also just a letter code that identifies you. If you're one of those people who hates staying home on the weekend, check out the local bowling alley. "]
         self.text_position = 0
